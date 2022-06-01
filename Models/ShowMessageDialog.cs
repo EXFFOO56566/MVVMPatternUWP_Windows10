4a081585353eb3ce5056e24387793d3bfdd9ba40
tochyodikwa <odikwatochy233@gmail.com>
2022-06-01 13:15:47 -0700

    first commit

---
+++ Models/ShowMessageDialog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVNpattern.Models
+{
+    public class ShowMessageDialog
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ ViewModels/StartPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using MVVNpattern.Models;
+
+namespace MVVNpattern.ViewModels
+{
+    public class StartPageViewModel : ViewModelBase
+    {
+        private bool _isLoading = false;
+        public bool IsLoading
+        {
+            get
+            {
+                return _isLoading;
+            }
+            set
+            {
+                _isLoading = value;
+                RaisePropertyChanged("IsLoading");
+
+            }
+        }
+        private string _title;
+        public string Title
+        {
+
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    RaisePropertyChanged("Title");
+                }
+            }
+        }
+
+        public StartPageViewModel()
+        {
+            Title = "Hello Tochy";
+        }
+    }
+}
